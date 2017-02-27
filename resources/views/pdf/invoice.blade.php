a063edbde566865fa2a4b2850d2bdd55c1742264
DigvijaySS <suryawanshidigvijay@gmail.com>
2017-02-27 10:46:22 +0530

    Investor Address added in Share Certificate

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -18,7 +18,7 @@
 	Date: {{ $investment->created_at->toFormattedDateString() }}
 	<br><br><br><br>
 
-	This is to certify {{$investment->user->first_name}} {{$investment->user->last_name}} owns {{$investment->amount}} shares of @if($investment->project->projectspvdetail){{$investment->project->projectspvdetail->first()->spv_name}}@else Estate Baron @endif.
+	This is to certify {{$investment->user->first_name}} {{$investment->user->last_name}}@if($investment->user->line_1) of  {{$investment->user->line_1}}, @if($investment->user->line_2){{$investment->user->line_2}},@endif {{$investment->user->city}}, {{$investment->user->state}}, {{$investment->user->postal_code}}@endif owns {{$investment->amount}} shares of @if($investment->project->projectspvdetail){{$investment->project->projectspvdetail->first()->spv_name}}@else Estate Baron @endif.
 	<br><br><br><br>
 
 	<img src="{{public_path().'/'.$investment->project->media->where('type', 'spv_md_sign_image')->first()->path}}" width="150">
