024006173f2b79c66cc585c41b6c4038b779b453
Sujit Mahavarkar <s.mahavarkar@gmail.com>
2017-03-02 10:02:37 +0530

    floating bar

--- resources/views/projects/gform/thankyou.blade.php
+++ resources/views/projects/gform/thankyou.blade.php
@@ -18,7 +18,7 @@
 				<div class="row">
 					<div class="col-md-12 text-center">
 						<h2>
-							Thank you <br> Please deposit ${{number_format($amount)}} to
+							Thank you Sujit <br> Please deposit ${{number_format($amount)}} to
 						</h2>
 					</div>
 				</div>
