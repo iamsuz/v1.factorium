f6495dde8beca808fc95bf39ac985dce490ef26c
Sujit Mahavarkar <s.mahavarkar@gmail.com>
2017-03-02 09:46:51 +0530

    floating bar

--- resources/views/projects/gform/thankyou.blade.php
+++ resources/views/projects/gform/thankyou.blade.php
@@ -6,37 +6,41 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
 	<title>Thank You for expressing interest</title>
-
+	{{!! Html::style('/css/app2.css') !!}}
 	{!! Html::style('/css/bootstrap.min.css') !!}
 	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,700,200italic,400italic,700italic' rel='stylesheet' type='text/css'>
 </head>
 <body>
 	<div class="container">
 		<br>
-		<div class="row">
-			<div class="col-md-12 text-center">
-				<h2>
-					Thank you <br> Please deposit ${{number_format($amount)}} to
-				</h2>
+		<section id="section-colors-left" class="color-panel-right panel-open-left center" style="opacity: .8;">
+			<div class="color-wrap-left">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<h2>
+							Thank you <br> Please deposit ${{number_format($amount)}} to
+						</h2>
+					</div>
+				</div>
+				<br>
+				@if($project->investment)
+				@if($project->investment->bank)
+				<div class="row">
+					<div class="col-md-offset-3 col-md-8 text-justify">
+						<table class="table table-bordered">
+							<tr><td>Bank</td><td>{!!$project->investment->bank!!}</td></tr>
+							<tr><td>Account Name</td><td>{!!$project->investment->bank_account_name!!}</td></tr>
+							<tr><td>BSB </td><td>{!!$project->investment->bsb!!}</td></tr>
+							<tr><td>Account No</td><td>{!!$project->investment->bank_account_number!!}</td></tr>
+							<tr><td>Reference</td><td>{!!$project->investment->bank_reference!!}</td></tr>
+						</table>
+					</div>
+				</div>
+				<br>
+				@endif
+				@endif
 			</div>
-		</div>
-		<br>
-		@if($project->investment)
-		@if($project->investment->bank)
-		<div class="row">
-			<div class="col-md-offset-3 col-md-8 text-justify">
-				<table class="table table-bordered">
-					<tr><td>Bank</td><td>{!!$project->investment->bank!!}</td></tr>
-					<tr><td>Account Name</td><td>{!!$project->investment->bank_account_name!!}</td></tr>
-					<tr><td>BSB </td><td>{!!$project->investment->bsb!!}</td></tr>
-					<tr><td>Account No</td><td>{!!$project->investment->bank_account_number!!}</td></tr>
-					<tr><td>Reference</td><td>{!!$project->investment->bank_reference!!}</td></tr>
-				</table>
-			</div>
-		</div>
-		<br>
-		@endif
-		@endif
+		</section>
 		<div class="row">
 			<div class="col-md-12 text-center">
 				<a href="javascript:void(0);" onclick="top.window.location.href='{{route('home')}}';">BACK TO HOME</a>
