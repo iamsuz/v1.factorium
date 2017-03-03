b58919591ac2a8232f23f33ad5d9f0ee4f6c30b9
DigvijaySS <suryawanshidigvijay@gmail.com>
2017-03-03 15:39:52 +0530

    Google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,6 +56,18 @@
     <script src="https://oss.maxcdn.com/libs/Html5shiv/3.7.0/Html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
+
+    <!-- Google analytics -->
+    <script>
+       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+       ga('create', 'UA-73934803-1', 'auto');
+       ga('send', 'pageview');
+    </script>
+
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 </head>
 <body data-spy="scroll">
