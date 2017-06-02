29818d16997d1cb8f3e39b7f9d7c760a7ced8f2c
DigvijaySS <suryawanshidigvijay@gmail.com>
2017-06-03 04:55:14 +0530

    delete testimonial fix to delete testimonial is count is less than 3

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -928,24 +928,34 @@
     @else
     <div class="row" style="display:-webkit-box;-webkit-box-pack:center;-webkit-box-align:center;">
       @foreach($testimonials as $testimonial)
-      <div class="col-md-4" style="margin-bottom: 3%;">
-      <div style="border-left: 5px solid #ddd;">
-        <center>
-          <img src="{{asset($testimonial->user_image_url)}}" class="img-circle"  width="200" height="200">
-        </center>
-      </div>
-      <span style="font-style: italic; float: right;"><b>{{$testimonial->user_name}}</b></span><br>
-      <span style="font-style: italic; float: right;">{{$testimonial->user_summary}}</span><br>
-      <div style="clear: both; padding-bottom: 5px;"></div>
-      <p class="text-justify">
-        <i class="fa fa-quote-left fa-3x fa-pull-left" style="color:#aaa;"></i>
-        @if(strlen($testimonial->user_content) > 150)
-        {{substr($testimonial->user_content, 0, 150)}}<span class="ellipsis">...</span><span class="moreText" style="display: none;">{{substr($testimonial->user_content, 150)}}</span><span class="read-more read-more-style"><small><small><u>read more</u></small></small></span>
+
+      <div class="col-md-4" style="margin-bottom: 3% 0%;">
+        @if(Auth::guest())
         @else
-        {{$testimonial->user_content}}
+        @if($admin_access == 1)
+          {!! Form::open(array('route'=>['pages.testimonial.delete'], 'class'=>'form-horizontal', 'role'=>'form')) !!}
+          <input type="hidden" name="testimonial_id" value="{{$testimonial->id}}">
+          <input type="submit" class="btn btn-primary btn-sm" name="delete_testimonial" value="delete">
+          {!! Form::close() !!}
         @endif
-      </p>
-    </div>
+        @endif
+        <div style="border-left: 5px solid #ddd;">
+          <center>
+            <img src="{{asset($testimonial->user_image_url)}}" class="img-circle"  width="200" height="200">
+          </center>
+        </div>
+        <span style="font-style: italic; float: right;"><b>{{$testimonial->user_name}}</b></span><br>
+        <span style="font-style: italic; float: right;">{{$testimonial->user_summary}}</span><br>
+        <div style="clear: both; padding-bottom: 5px;"></div>
+        <p class="text-justify">
+          <i class="fa fa-quote-left fa-3x fa-pull-left" style="color:#aaa;"></i>
+          @if(strlen($testimonial->user_content) > 150)
+          {{substr($testimonial->user_content, 0, 150)}}<span class="ellipsis">...</span><span class="moreText" style="display: none;">{{substr($testimonial->user_content, 150)}}</span><span class="read-more read-more-style"><small><small><u>read more</u></small></small></span>
+          @else
+          {{$testimonial->user_content}}
+          @endif
+        </p>
+      </div>
       @endforeach
     </div>
     @endif
