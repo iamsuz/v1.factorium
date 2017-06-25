5e644835662c135ca8748392fe275421e791daec
DigvijaySS <suryawanshidigvijay@gmail.com>
2017-06-25 18:05:22 +0530

    issue share certificate error fix if the spv logo is not defined

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -3,8 +3,10 @@
 	<h1>Share Certificate</h1>
 	<br><br>
 
+	@if($investment->project->media->where('type', 'spv_logo_image')->first())
 	<img src="{{public_path().'/'.$investment->project->media->where('type', 'spv_logo_image')->first()->path}}" width="300">
 	<br><br>
+	@endif
 	@if($investment->project->projectspvdetail){{$investment->project->projectspvdetail->first()->spv_name}}@else Estate Baron @endif
 	<br>
 
@@ -21,8 +23,10 @@
 	This is to certify @if($investment->investing_as == 'Individual Investor') {{$investment->user->first_name}} {{$investment->user->last_name}} @elseif($investment->investing_as == 'Joint Investor') {{$investment->user->first_name}} {{$investment->user->last_name}} and {{$investing->joint_investor_first_name}} {{$investing->joint_investor_last_name}} @elseif($investment->investing_as == 'Trust or Company') {{$investing->investing_company}} @else {{$investment->user->first_name}} {{$investment->user->last_name}}@endif @if($investment->user->line_1) of  {{$investment->user->line_1}}, @if($investment->user->line_2){{$investment->user->line_2}},@endif {{$investment->user->city}}, {{$investment->user->state}}, {{$investment->user->postal_code}}@endif owns {{$investment->amount}} redeemable preference shares of @if($investment->project->projectspvdetail){{$investment->project->projectspvdetail->first()->spv_name}}@else Estate Baron @endif numbered {{$shareStart}} to {{$shareEnd}}.
 	<br><br><br><br>
 
+	@if($investment->project->media->where('type', 'spv_md_sign_image')->first())
 	<img src="{{public_path().'/'.$investment->project->media->where('type', 'spv_md_sign_image')->first()->path}}" width="150">
 	<br>
+	@endif
 	@if($investment->project->projectspvdetail){{$investment->project->projectspvdetail->first()->spv_md_name}}@else Moresh Kokane @endif
 	<br>
 	Managing Director
