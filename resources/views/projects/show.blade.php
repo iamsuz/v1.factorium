33dc74bc180bc4a5da551020b87f8e61c79da229
Viraj Kokane <virajkokane123@gmail.com>
2018-05-07 01:53:52 +0530

    New changes custom project feature

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+@extends('layouts.project')
 @section('title-section')
 {{$project->title}} crowdfunding with just ${{(int)$project->investment->minimum_accepted_amount}}
 @stop
