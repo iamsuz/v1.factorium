49dff18b870d524ca1892866d8120d73319dcf12
Viraj Kokane <virajkokane123@gmail.com>
2018-08-16 15:17:47 +0530

    Added fix for login bonus variable eoi issue

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -107,9 +107,9 @@ class UserAuthController extends Controller
     public function authenticateEoi(UserAuthRequest $request,AppMailer $mailer)
     {
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'active'=>1], $request->remember)) {
+            $loginBonus = rand(1, 50);
             if(Auth::user()->last_login){
-                if(!Auth::user()->last_login->gt(\Carbon\Carbon::now()->subDays(1))){
-                    $loginBonus = rand(1, 50);
+                if(!Auth::user()->last_login->gt(\Carbon\Carbon::now()->subDays(1))){                    
                     Credit::create([
                         'user_id' => Auth::user()->id,
                         'amount' => $loginBonus,
