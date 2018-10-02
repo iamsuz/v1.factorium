ceca6e24e835764dc37fb69f4976b3c61b08a29e
Viraj Kokane <virajkokane123@gmail.com>
2018-10-02 22:56:02 +0530

    Migration file fix for konkrete

--- database/migrations/2018_09_19_195311_add_konkrete_allocation_columns_to_site_configurations_table.php
+++ database/migrations/2018_09_19_195311_add_konkrete_allocation_columns_to_site_configurations_table.php
@@ -13,12 +13,12 @@ class AddKonkreteAllocationColumnsToSiteConfigurationsTable extends Migration
     public function up()
     {
         Schema::table('site_configurations', function (Blueprint $table) {
-            $table->string('daily_login_bonus_konkrete')->default('10');
-            $table->string('user_sign_up_konkrete')->default('100');
-            $table->string('kyc_upload_konkrete')->default('200');
-            $table->string('kyc_approval_konkrete')->default('200');
-            $table->string('referrer_konkrete')->default('200');
-            $table->string('referee_konkrete')->default('200');
+            $table->string('daily_login_bonus_konkrete');
+            $table->string('user_sign_up_konkrete');
+            $table->string('kyc_upload_konkrete');
+            $table->string('kyc_approval_konkrete');
+            $table->string('referrer_konkrete');
+            $table->string('referee_konkrete');
         });
     }
 
@@ -34,7 +34,7 @@ class AddKonkreteAllocationColumnsToSiteConfigurationsTable extends Migration
             $table->dropColumn('user_sign_up_konkrete');
             $table->dropColumn('kyc_upload_konkrete');
             $table->dropColumn('kyc_approval_konkrete');
-            $table->dropColumn('referral_konkrete');
+            $table->dropColumn('referrer_konkrete');
             $table->dropColumn('referee_konkrete');
         });
     }
