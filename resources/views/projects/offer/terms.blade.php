33cd7c92968133ac6c6c5a68fa7d48a1ec542b7c
Sujit Mahavarkar <s.mahavarkar@gmail.com>
2018-10-25 18:59:52 +0700

    create abbreviation for prospectus name

--- resources/views/projects/offer/terms.blade.php
+++ resources/views/projects/offer/terms.blade.php
@@ -8,9 +8,21 @@
           @if($project->investment){!! $project->investment->PDS_part_1_link !!}@endif
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-primary" data-dismiss="modal" id="terms_accepted_button">I have read the @if($project->project_prospectus_text) {{$project->project_prospectus_text}} @else prospectus @endif, take me to the application form</button>
+          <button type="button" class="btn btn-primary" data-dismiss="modal" id="terms_accepted_button">I have read the @if($project->project_prospectus_text) <abbr title="{{$project->project_prospectus_text}}"><span id="abbrev"></span></abbr> @else prospectus @endif, take me to the application form</button>
         </div>
       </div>
-
+      <script>
+        $(document).ready(function () {
+            var str1 = "{{$project->project_prospectus_text}}";
+            var split_names = str1.trim().split(" ");
+            if (split_names.length > 1) {
+              var newResult = [];
+              for(var i=0;i < split_names.length; i++){
+                newResult.push(split_names[i].charAt(0));
+              }
+              $('#abbrev').html(newResult);
+            }
+        });
+      </script>
     </div>
   </div>
