da2decb5fa3537cca32e256f4a71718fb88f6f22
Viraj Kokane <virajkokane123@gmail.com>
2018-11-17 15:10:00 +0530

    KYC verification mail changes

--- resources/views/emails/verifyNotification.blade.php
+++ resources/views/emails/verifyNotification.blade.php
@@ -581,7 +581,7 @@ Note - {{$idimages->joint_id_comment}}
 @endif
 @else
 We have successfully verified your KYC documents.
-<br> <br>You may now start investing in projects.
+<br>{{--  <br>You may now start investing in projects. --}}
 @endif
 </span></span></span>
 &nbsp;</div>
