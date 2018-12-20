a561c18d8ff6d7e419cb793603f3d2875722f539
Digvijay Suryawanshi <suryawanshidigvijay@gmail.com>
2018-12-21 01:58:08 +0530

    Fixed - Safari users going to home page of whitelabel site

--- public/setSession.php
+++ public/setSession.php
@@ -2,5 +2,6 @@
 header('P3P: CP="CURa ADMa DEVa PSAo PSDo OUR BUS UNI PUR INT DEM STA PRE COM NAV OTC NOI DSP COR"');
 session_start();
 $_SESSION['safari'] = 1;
-header('location:/index.php');
+$redirectTo = $_REQUEST['redirect'] ?? '/index.php';
+header('location:'.$redirectTo);
 ?>
