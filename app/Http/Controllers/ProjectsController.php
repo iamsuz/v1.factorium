55e2770f8f7b7cca20246efa68c32f234972d3c1
Digvijay Suryawanshi <suryawanshidigvijay@gmail.com>
2019-09-04 17:07:27 +0530

    Asking price refresh fix for non auth users

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -44,7 +44,7 @@ class ProjectsController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['index', 'show','redirectingfromproject', 'gform', 'gformRedirects','showEoiInterest']]);
+        $this->middleware('auth', ['except' => ['index', 'show','redirectingfromproject', 'gform', 'gformRedirects','showEoiInterest', 'refreshAskingAmount']]);
         $this->uri = env('KONKRETE_IP', 'http://localhost:5050');
         if(isset(SiteConfiguration::where('project_site', url())->first()->audk_default_project_id)){
             $this->audkID = SiteConfiguration::where('project_site', url())->first()->audk_default_project_id;
