7addd5ee9327fd1f8cfd450c68bfef2f76cfaaa0
Sujit Mahavarkar <s.mahavarkar@gmail.com>
2019-09-12 08:38:05 +0530

    UI changes for buy AUDC

--- resources/views/users/buyAudc.blade.php
+++ resources/views/users/buyAudc.blade.php
@@ -40,6 +40,8 @@
 							<input type="submit" class="btn btn-primary form-control" name="submit" value="Buy">
 						</div>
 					</div>
+					<p><small><small>Your AUDC purchase is operated as an exempt Non Cash internal payment facility. Each AUDC represents 1 Australian Cent or 1/100th of an Australian Dollar. No more than 1000 Million AUDC (or $10 Million AUD) are under circulation and no user is allowed to hold more than $1000 worth of AUDC.
+						We use AUDC to enable cash equivalent movements within the platform. You may redeem any AUDC held by you from us in favor of Australian Dollars at any time. We will process any payments promptly after any AML/CTF/KYC obligations are met. We hold an equivalent amount of Australian Dollars in trust with us to the value of AUDC under circulation ensuring it is always maintained at a one to one peg. We also publish bank statements confirming the balance every 7 days.</small></small></p>
 				</form>
 			</div>
 			<div>
@@ -61,7 +63,7 @@
 								</td>
 								<td>@if($investor->pivot->accepted == 1) <i>Accepted</i> @else Yet to accept @endif</td>
 								<td>
-									@if($investor->pivot->transaction_hash){{$investor->pivot->transaction_hash}} @else Waiting for approval @endif
+									@if($investor->pivot->transaction_hash){{$investor->pivot->transaction_hash}} @else Waiting for approval <a href="#" data-toggle="tooltip" data-placement="right" title="For approval please transfer {{$investor->pivot->amount}} AUD to listed bank details"><i class="fa fa-question-circle"></i></a> @endif
 								</td>
 							</tr>
 							@endforeach
