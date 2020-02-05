49fb90e81af2240ca82318f6cf22763bcc9cfeef
Sujit Mahavarkar <s.mahavarkar@gmail.com>
2020-02-05 12:53:14 +1100

    adds browser solc js to the local folder as it was unsecure

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -417,7 +417,7 @@ $_SESSION['code'] = md5(microtime(true));
     })();
 </script>
 <script src="https://cdn.jsdelivr.net/gh/ethereum/web3.js@1.0.0-beta.36/dist/web3.min.js" integrity="sha256-nWBTbvxhJgjslRyuAKJHK+XcZPlCnmIAAMixz6EefVk=" crossorigin="anonymous"></script>
-<script src="http://code.dappbench.com/browser-solc.min.js" type="text/javascript"></script>
+<script src="/js/browser-solc.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="/assets/abi/smartInvoiceABI.js"></script>
 <script type="text/javascript" src="/assets/abi/daiABI.js"></script>
 <script type="text/javascript" src="/assets/js/app.js"></script>
