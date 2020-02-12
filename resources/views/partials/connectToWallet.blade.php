82c67d5352d1a635b930bf298771786a7b02b60f
Sujit Mahavarkar <s.mahavarkar@gmail.com>
2020-02-12 15:56:20 +1100

    fixes modal image for connect metamask modal

--- resources/views/partials/connectToWallet.blade.php
+++ resources/views/partials/connectToWallet.blade.php
@@ -7,17 +7,17 @@
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h4 class="modal-title">Modal Header</h4>
       </div>
-      <div class="modal-body" style="height: 500px;">
+      <div class="modal-body" style="height: 550px;">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <br><br><br>
+        <br>
         <div class="row text-center">
           <div class="col-md-8 col-md-offset-2">
-            <img src="/assets/images/konkrete_logo_dark.png" height="90px" width="90px" style="text-shadow: 10px 10px 5px grey;">
+            <img src="/assets/images/FactoriumIcon.png" class="img-responsive" style="text-shadow: 10px 10px 5px grey;">
             <h3>Connect Wallet</h3>
             <p>To start using Factorium</p>
           </div>
         </div>
-        <br><br><br><br>
+        <br><br><br>
         <div class="row">
           <div class="col-md-8 col-md-offset-2">
             <button class="btn btn-lg btn-block buy-now-btn btn-primary" id="metamaskConnect"><img src="/assets/images/metamask-logo-png-transparent.png" width="25px">&nbsp;&nbsp;&nbsp; Metamask</button>
